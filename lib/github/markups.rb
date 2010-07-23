5d5bc19e633ac22886f5afe86f59871a3ab882ba
Tom Preston-Werner <tom@mojombo.com>
2010-07-23 15:44:28 -0700

    Include AsciiDoc header with document and remove asciidoc command clutter.

--- lib/github/markups.rb
+++ lib/github/markups.rb
@@ -1,34 +1,41 @@
-markup(:markdown, /md|mkdn?|mdown|markdown/) do |content|
-  Markdown.new(content).to_html
+# asciidoc
+command('asciidoc --safe --backend=xhtml11 -o - -', /asciidoc/) do |rendered|
+  if rendered =~ /<body>\n(.*)<div id="footnotes">/m
+    $1
+  end
 end
 
-markup(:redcloth, /textile/) do |content|
-  RedCloth.new(content).to_html
-end
-
-markup('github/markup/rdoc', /rdoc/) do |content|
-  GitHub::Markup::RDoc.new(content).to_html
-end
-
-markup('org-ruby', /org/) do |content|
-  Orgmode::Parser.new(content).to_html
-end
-
+# creole
 markup(:creole, /creole/) do |content|
   Creole.creolize(content)
 end
 
-command(:rest2html, /re?st(\.txt)?/)
+# markdown
+markup(:markdown, /md|mkdn?|mdown|markdown/) do |content|
+  Markdown.new(content).to_html
+end
 
-command('asciidoc -s --backend=xhtml11 -o - -', /asciidoc/)
+# org
+markup('org-ruby', /org/) do |content|
+  Orgmode::Parser.new(content).to_html
+end
 
-# pod2html is nice enough to generate a full-on HTML document for us,
-# so we return the favor by ripping out the good parts.
-#
-# Any block passed to `command` will be handed the command's STDOUT for
-# post processing.
+# pod
 command("/usr/bin/env perl -MPod::Simple::HTML -e Pod::Simple::HTML::go", /pod/) do |rendered|
   if rendered =~ /<!-- start doc -->\s*(.+)\s*<!-- end doc -->/mi
     $1
   end
+end
+
+# rdoc
+markup('github/markup/rdoc', /rdoc/) do |content|
+  GitHub::Markup::RDoc.new(content).to_html
+end
+
+# rest
+command(:rest2html, /re?st(\.txt)?/)
+
+# textile
+markup(:redcloth, /textile/) do |content|
+  RedCloth.new(content).to_html
 end
