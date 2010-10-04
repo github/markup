3b16c914e76bb0c1b4d31c9ad6e20dd80e91ea8e
Joel Moss <joel@developwithstyle.com>
2010-10-04 11:54:55 +0100

    Added Neapolitan

--- lib/github/markups.rb
+++ lib/github/markups.rb
@@ -18,6 +18,10 @@ markup(:creole, /creole/) do |content|
   Creole.creolize(content)
 end
 
+markup(:neapolitan, /neapolitan|np|npt|choc/) do |content|
+  Neapolitan::Template.new(content).render.to_s
+end
+
 command(:rest2html, /re?st(\.txt)?/)
 
 command('asciidoc -s --backend=xhtml11 -o - -', /asciidoc/)
