3fde31839334a124f78339cb4e20f98788601163
Douglas Campos <qmx@qmx.me>
2010-12-30 01:26:29 -0200

    added mediawiki support

--- lib/github/markups.rb
+++ lib/github/markups.rb
@@ -18,6 +18,10 @@ markup(:creole, /creole/) do |content|
   Creole.creolize(content)
 end
 
+markup(:wikicloth, /mediawiki|wiki/) do |content|
+  WikiCloth::WikiCloth.new({:data => content}).to_html
+end
+
 command(:rest2html, /re?st(\.txt)?/)
 
 command('asciidoc -s --backend=xhtml11 -o - -', /asciidoc/)
