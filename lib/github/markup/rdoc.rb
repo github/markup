bd2982ee38a575679de87f883f65bc07c2cd2916
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2013-08-26 12:47:22 +0000

    Fix Rdoc render with ruby 2.0

--- lib/github/markup/rdoc.rb
+++ lib/github/markup/rdoc.rb
@@ -9,7 +9,11 @@ module GitHub
       end
 
       def to_html
-        h = ::RDoc::Markup::ToHtml.new
+        if Gem::Version.new(::RDoc::VERSION) < Gem::Version.new('4.0.0')
+          h = ::RDoc::Markup::ToHtml.new
+        else
+          h = ::RDoc::Markup::ToHtml.new(::RDoc::Options.new)
+        end
         h.convert(@content)
       end
     end
