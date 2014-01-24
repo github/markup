2eb3c2dd015c9614416ea606486e3954289b8c46
David E. Wheeler <david@justatheory.com>
2014-01-24 15:43:10 -0800

    Maybe need to explicitly send the command to STDOUT?

--- lib/github/markups.rb
+++ lib/github/markups.rb
@@ -32,5 +32,5 @@ end
 
 command(:rest2html, /re?st(\.txt)?/)
 
-command("/usr/bin/env perldoc -MPod::Simple::XHTML -w html_header: -w html_footer:", /pod/)
+command("/usr/bin/env perldoc -MPod::Simple::XHTML -w html_header: -w html_footer: -T", /pod/)
 
