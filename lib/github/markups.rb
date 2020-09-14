01a27d7b40e6fd0aa8d0b5abbba5e949379f3a65
Jeremy Zafran <jzafran@users.noreply.github.com>
2020-09-14 00:02:59 -0400

    Run rest2html with python to allow use of Python 3

--- lib/github/markups.rb
+++ lib/github/markups.rb
@@ -49,7 +49,7 @@ end
 
 command(
   ::GitHub::Markups::MARKUP_RST,
-  "python2 -S #{Shellwords.escape(File.dirname(__FILE__))}/commands/rest2html",
+  "python -S #{Shellwords.escape(File.dirname(__FILE__))}/commands/rest2html",
   /re?st(\.txt)?/,
   ["reStructuredText"],
   "restructuredtext"
