1bc2ae25c3e13bfefb5adcb79e7f9be9522f5d3c
Fuzzwah <fuzzwah@github.com>
2024-02-29 02:17:40 +0000

    back to pandoc-ruby

--- lib/github/markups.rb
+++ lib/github/markups.rb
@@ -1,7 +1,7 @@
 require "github/markup/markdown"
 require "github/markup/rdoc"
 require "shellwords"
-require "paru/pandoc"
+require 'pandoc-ruby'
 
 markup_impl(::GitHub::Markups::MARKUP_MARKDOWN, ::GitHub::Markup::Markdown.new)
 
@@ -48,12 +48,8 @@ markup(::GitHub::Markups::MARKUP_ASCIIDOC, :asciidoctor, /adoc|asc(iidoc)?/, ["A
   Asciidoctor.convert(content, :safe => :secure, :attributes => attributes)
 end
 
-markup(::GitHub::Markups::MARKUP_RST, :paru, /re?st(\.txt)?/, ["rst"]) do |filename, content, options: {}|
-  output = Paru::Pandoc.new do
-    from "rst"
-    to "html"
-  end << content
-  puts output
+markup(::GitHub::Markups::MARKUP_RST, :pandoc-ruby, /re?st(\.txt)?/, ["reStructuredText"]) do |filename, content, options: {}|
+  PandocRuby.new(content, from: "rst").to_html
 end
 
 command(::GitHub::Markups::MARKUP_POD6, :pod62html, /pod6/, ["Pod 6"], "pod6")
